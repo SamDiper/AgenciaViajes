bee818ebd67a835ae32a654ca53b3e22cbbb9a3c
SamDiper <samueldiazp.dev@gmail.com>
2026-09-20 14:09:11 -0500

    actualización del security y el index para los estilos y animaciones; adición de un destino y actualización de imagenes en el sql

--- AgenciaViajes/src/main/java/com/example/AgenciaViajes/config/SecurityConfig.java
+++ AgenciaViajes/src/main/java/com/example/AgenciaViajes/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/", "/home",
                                 "/login", "/clientes", "/clientes/guardar", "/auth/**",
-                                "/css/**", "/js/**", "/h2-console/**"
+                                "/css/**", "/js/**", "/assets/**", "/pages/**", "/api/**", "/images/**", "/h2-console/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
--- AgenciaViajes/src/main/resources/data.sql
+++ AgenciaViajes/src/main/resources/data.sql
@@ -2,7 +2,10 @@ INSERT INTO categorias_destino (nombre) VALUES ('Playa');
 INSERT INTO categorias_destino (nombre) VALUES ('Montaña');
 
 INSERT INTO destinos (nombre, pais, ciudad, descripcion, id_categoria, imagen_url, estado, fecha_registro)
-VALUES ('Cartagena de Indias', 'Colombia', 'Cartagena', 'Ciudad amurallada con playas caribeñas.', 1, 'https://images.unsplash.com/photo-1544644181-1484b3fdfc62', 'ACTIVO', CURRENT_TIMESTAMP);
+VALUES ('Cartagena de Indias', 'Colombia', 'Cartagena', 'Ciudad amurallada con playas caribeñas.', 1, 'https://plus.unsplash.com/premium_photo-1697730195920-86bc1a6eeab0', 'ACTIVO', CURRENT_TIMESTAMP);
 
 INSERT INTO destinos (nombre, pais, ciudad, descripcion, id_categoria, imagen_url, estado, fecha_registro)
-VALUES ('Salento', 'Colombia', 'Salento', 'Pueblo cafetero rodeado de montañas.', 2, 'https://images.unsplash.com/photo-1533105079780-92b9be482077', 'ACTIVO', CURRENT_TIMESTAMP);
+VALUES ('Salento', 'Colombia', 'Salento', 'Pueblo cafetero rodeado de montañas.', 2, 'https://images.unsplash.com/photo-1697818634560-27ddca612f88', 'ACTIVO', CURRENT_TIMESTAMP);
+
+INSERT INTO destinos (nombre, pais, ciudad, descripcion, id_categoria, imagen_url, estado, fecha_registro)
+VALUES ('Tayrona','Colombia','Santa Marta','Parque conocido por sus playas, selva tropical y paisajes naturales.',1,'https://images.unsplash.com/photo-1635079552384-dd8adecd8a7c','ACTIVO',CURRENT_TIMESTAMP);
