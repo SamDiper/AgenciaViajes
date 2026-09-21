d6060fa03e3007a53685f4b74085ceff71281826
SamDiper <samueldiazp.dev@gmail.com>
2026-09-21 16:00:20 -0500

    Listados de las entidades adentro del modulo configuración admin

---
+++ AgenciaViajes/src/main/java/com/example/AgenciaViajes/repositorio/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.example.AgenciaViajes.repositorio;
+
+import com.example.AgenciaViajes.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByCorreo(String correo);
+
+    boolean existsByCorreo(String correo);
+}
